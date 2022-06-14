439bc77aec25d48da3f5e8664beeae034c8165d1
Alex Harvey <alex.harvey@shopify.com>
2022-06-14 11:26:45 -0300

    moved subcomponent forEach into the utils and removed empty returns

--- src/utilities.ts
+++ src/utilities.ts
@@ -3,59 +3,57 @@ import * as ts from 'typescript';
 import { ComponentDoc, Parser, ParserOptions } from './parser';
 
 const hasSameDisplayName = (comp: ComponentDoc) => (
-  compDoc: ComponentDoc
+    compDoc: ComponentDoc
 ): boolean => {
-  return compDoc!.displayName === comp!.displayName;
+    return compDoc!.displayName === comp!.displayName;
 };
 
 function isComponentInList(
-  componentDocs: ComponentDoc[],
-  comp: ComponentDoc
+    componentDocs: ComponentDoc[],
+    comp: ComponentDoc
 ): boolean {
-  return !!componentDocs.find(hasSameDisplayName(comp));
+    return !!componentDocs.find(hasSameDisplayName(comp));
 }
 
 export function removeDuplicateDocs(componentDocs: ComponentDoc[]) {
-  return componentDocs.reduce(
-    (acc, comp) => (isComponentInList(acc, comp) ? acc : [...acc, comp]),
-    [] as ComponentDoc[]
-  );
+    return componentDocs.reduce(
+        (acc, comp) => (isComponentInList(acc, comp) ? acc : [...acc, comp]),
+        [] as ComponentDoc[]
+    );
 }
 
 export const documentSubComponent = (
-  parser: Parser,
-  checker: ts.TypeChecker,
-  sourceFile: ts.SourceFile,
-  parserOpts: ParserOptions,
-  exp: ts.Symbol
+    parser: Parser,
+    checker: ts.TypeChecker,
+    sourceFile: ts.SourceFile,
+    parserOpts: ParserOptions,
+    exp: ts.Symbol
 ) => (symbol: ts.Symbol): ComponentDoc | null => {
-  // TODO investigate further
-  // checking memory addresses match????
-  if (!(symbol.flags & ts.SymbolFlags.Prototype)) {
+    // TODO investigate further
+    // checking memory addresses match????
     if (symbol.flags & ts.SymbolFlags.Method) {
-      const signature = parser.getCallSignature(symbol);
-      const returnType = checker.typeToString(signature.getReturnType());
+        const signature = parser.getCallSignature(symbol);
+        const returnType = checker.typeToString(signature.getReturnType());
 
-      if (returnType === 'Element') {
-        const doc = parser.getComponentInfo(
-          symbol,
-          sourceFile,
-          parserOpts.componentNameResolver,
-          parserOpts.customComponentTypes
-        );
+        if (returnType === 'Element') {
+            const doc = parser.getComponentInfo(
+                symbol,
+                sourceFile,
+                parserOpts.componentNameResolver,
+                parserOpts.customComponentTypes
+            );
 
-        if (doc) {
-          const prefix =
-            exp.escapedName === 'default' ? '' : `${exp.escapedName}.`;
+            if (doc) {
+                const prefix =
+                    exp.escapedName === 'default' ? '' : `${exp.escapedName}.`;
 
-          return {
-            ...doc,
-            displayName: `${prefix}${symbol.escapedName}`
-          };
+                return {
+                    ...doc,
+                    displayName: `${prefix}${symbol.escapedName}`
+                };
+            }
         }
-      }
     }
-  }
 
-  return null;
+    return null;
 };
